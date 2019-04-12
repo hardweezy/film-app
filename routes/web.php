0089e8e23cbe831da84ebe52038d9878ec7a2559
Solomon Omokehinde <solomon@apprise.co.za>
2019-04-12 21:23:25 +0200

    Seeder Files added, routes were defined too.

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,19 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::group(['prefix' => '/films'], function () {
+  Route::get('/', ['as' => 'films', 'uses' => 'FilmsController@feed']);
+  Route::get('create', ['as' => 'films.create', 'uses' => 'FilmsController@create']);
+  Route::get('edit/{film}', ['as' => 'films.edit', 'uses' => 'FilmsController@edit']);
+  Route::post('store', ['as' => 'films.store', 'uses' => 'FilmsController@store']);
+  Route::get('{slug}', ['as' => 'films.show', 'uses' => 'FilmsController@show']);
+});
+
+Route::redirect('/', route('films'))->name('home');
+Auth::routes();
+
+Route::group(['middleware' => 'auth'], function () {
+  //comments
+  Route::post('/comment/add', ['as' => 'comment.add', 'uses' => 'CommentsController@addComment']);
+});
